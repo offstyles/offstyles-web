f2a7f1fa29b314831ccbb06cbff36ca9078e5601
Mia Woodhams <xenonpowered@gmail.com>
2025-06-22 20:01:01 +0100

    add times by map request & sample data

---
+++ src/sampleTimes.ts
@@ -0,0 +1,45 @@
+const sampleTimes = [{
+    _id: "685629946dbd08b4fb2bd0e5",
+    map: "bhop_bfur",
+    steamid: "tommy",
+    name: "tommy",
+    time: 15.77665512465078,
+    sync: 9.599981307983398,
+    strafes: 2416224402,
+    jumps: 3222771933,
+    date: 2460341901,
+    tickrate: 100,
+    replay: null,
+    style: 7,
+    is_invalid: false,
+    invalid_ref: null,
+    server: {
+      ip: "62.175.236.81",
+      name: "TvwkLBcQRwZieAD",
+      internal_id: "PWsHDFKRKRwPiBL",
+    }
+  },
+  {
+    _id: "685629946dbd08b4fb2bd0e5",
+    map: "bhop_bfur",
+    steamid: "mia",
+    name: "mia",
+    time: 16.12665512465078,
+    sync: 9.599981307983398,
+    strafes: 2416224402,
+    jumps: 3222771933,
+    date: 2460341901,
+    tickrate: 100,
+    replay: null,
+    style: 7,
+    is_invalid: false,
+    invalid_ref: null,
+    server: {
+      ip: "62.175.236.81",
+      name: "TvwkLBcQRwZieAD",
+      internal_id: "PWsHDFKRKRwPiBL",
+    }
+  },
+];
+
+export default sampleTimes;
