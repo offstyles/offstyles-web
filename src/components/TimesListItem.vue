f7a614dcedea7a366d48ec1efaf561c0e75a15ac
why-Mia <xenonpowered@gmail.com>
2025-07-06 20:37:08 +0100

    update styling for link columns

--- src/components/TimesListItem.vue
+++ src/components/TimesListItem.vue
@@ -19,8 +19,12 @@
 <template>
   <div class="grid os-grid-cols-auto p-1 px-2 bg-main-600 odd:bg-main-700">
     <div v-for="(col,index) in props.cols" :key="index" :class="col.classes">
-      <a v-if="col.link" :href="col.link(props.time)"><TimesListItemContent :col="col" :time="props.time" :wrTime="props.wrTime"></TimesListItemContent></a>
-      <div v-else><TimesListItemContent :col="col" :time="props.time" :wrTime="props.wrTime"></TimesListItemContent></div>
+      <a v-if="col.link" :href="col.link(props.time)" class="hover:underline">
+        <TimesListItemContent :col="col" :time="props.time" :wrTime="props.wrTime"></TimesListItemContent>
+      </a>
+      <div v-else>
+        <TimesListItemContent :col="col" :time="props.time" :wrTime="props.wrTime"></TimesListItemContent>
+      </div>
     </div>
   </div>
 </template>
