023a7a16d645055fd3b03bf760ede73f2b2d13b0
tommy <thefamousdoge@hotmail.com>
2025-08-08 13:44:15 -0400

    Rename Legit Scroll to Scroll

--- src/utils/styleFormat.ts
+++ src/utils/styleFormat.ts
@@ -5,7 +5,7 @@ const name = function(style : Style) {
     case Style.normal: return 'Normal';
     case Style.sideways: return 'Sideways';
     case Style.wonly: return 'W-Only';
-    case Style.legit_scroll: return 'Legit scroll';
+    case Style.legit_scroll: return 'Scroll';
     case Style.half_sideways: return 'Half-Sideways';
     case Style.a_d_only: return 'A/D-Only';
     case Style.segmented: return 'Segmented';
@@ -18,7 +18,7 @@ const styleFromName = function(name : string) {
     case 'normal': return Style.normal;
     case 'sideways': return Style.sideways;
     case 'w-only': return Style.wonly;
-    case 'legit scroll': return Style.legit_scroll;
+    case 'scroll': return Style.legit_scroll;
     case 'half-sideways': return Style.half_sideways;
     case 'a/d-only': return Style.a_d_only;
     case 'segmented': return Style.segmented;
