72dd7ed737c0d59fcccb2a7b187f5029016d5a9b
why-Mia <xenonpowered@gmail.com>
2025-09-15 00:17:56 +0100

    add support for new formatting to time list headings

--- src/components/TimeLists/TimesListHeading.vue
+++ src/components/TimeLists/TimesListHeading.vue
@@ -5,16 +5,30 @@
       cols: TimeListColumn[]
     }>();
 
+    const totalCols = computed(()=>{
+    return Math.max(...props.cols.map((v)=>{return v.col && v.colSpan ? v.col+v.colSpan : 1}));
+  })
+  const totalRows = computed(()=>{
+    return Math.max(...props.cols.map((v)=>{return v.row && v.rowSpan ? v.row+v.rowSpan : 1}));
+  })
+
   const colWidthsStyle = computed(()=>{
-    return props.cols.map((v)=>v.width ? v.width : 'auto').join(' ');
+    return props.cols.filter((v)=>v.row === undefined || v.row === 1) //only first row
+    .sort((a,b)=>a.col - b.col) //sort into correct col order
+    .map((v)=>v.width ? v.width : 'auto').join(' '); //make css
+  })
+
+  const rowWidthsStyle = computed(()=>{
+    return '1fr'; 
   })
 </script>
 
 
 <template>
-  <div class="grid os-grid-cols-auto px-1 bg-main-900 fw-700 text-xs pt-1.5 pb-2 text-gray-200">
-    <div v-for="(col,index) in props.cols" :key="index" class="grid-col px-1.5" :class="col.alignmentClasses">
-      <div :class="col.alignmentClasses">{{ col.label }}</div>
+  <div class="grid os-grid-cols-auto px-1 bg-main-900 fw-700 text-xs pt-1.5 pb-2 text-gray-200 items-center">
+    <div v-for="(col,index) in props.cols" :key="index" class="px-1.5" 
+    :style="`grid-column:${col.col} / span ${col.colSpan ?? 0}; grid-row:${col.row} / span ${col.rowSpan ?? 0};`">
+      <div v-if="!(col.row && col.row>1)" :class="`${col.alignmentClasses} ${col.row && col.row > 1 ? 'text-gray-500 leading-[1.17em]' : ''}`">{{ col.label }}</div>
     </div>
   </div>
 </template>
@@ -22,21 +36,26 @@
 <style scoped>
   .os-grid-cols-auto{
     grid-template-columns: v-bind('colWidthsStyle');
+    grid-template-rows: v-bind('rowWidthsStyle');
   }
-  @media(max-width:768px){
+  @media(max-width:767px){
     .os-grid-cols-auto{
-      grid-template-columns: repeat(2, 1fr);
+      grid-template-columns: 40% 60%;
       grid-template-rows: repeat(2, 1fr);
     }
-    .os-grid-cols-auto>.grid-col:nth-child(1n),
-    .os-grid-cols-auto>.grid-col:nth-child(1n)>div{
+    .os-grid-cols-auto>.grid-col{
+      width:100%;
+    }
+    .os-grid-cols-auto>.grid-col:nth-child(1n){
       text-align: left;
       justify-content: left;
     }
-    .os-grid-cols-auto>.grid-col:nth-child(2n),
-    .os-grid-cols-auto>.grid-col:nth-child(2n)>div{
+    .os-grid-cols-auto>.grid-col:nth-child(2n){
       text-align: right;
       justify-content: right;
+    }
+    .os-grid-cols-auto>.grid-col:nth-child(2n+1):last-child{
+      grid-column:span 2;
     }
   }
 </style>
--- src/components/TimeLists/TimesListItem.vue
+++ src/components/TimeLists/TimesListItem.vue
@@ -34,9 +34,9 @@
   })
 
   const colWidthsStyle = computed(()=>{
-    return props.cols.filter((v)=>v.row === undefined || v.row === 1)
-    .sort((a,b)=>a.col - b.col)
-    .map((v)=>v.width ? v.width : 'auto').join(' ');
+    return props.cols.filter((v)=>v.row === undefined || v.row === 1) //only first row
+    .sort((a,b)=>a.col - b.col) //sort into correct col order
+    .map((v)=>v.width ? v.width : 'auto').join(' '); //make css
   })
 
   const rowWidthsStyle = computed(()=>{
@@ -182,34 +182,6 @@
   .os-grid-cols-auto{
     grid-template-columns: v-bind('colWidthsStyle');
     grid-template-rows: v-bind('rowWidthsStyle');
-  }
-  @media(max-width:767px){
-    .os-grid-cols-auto{
-      grid-template-columns: 40% 60%;
-      grid-template-rows: repeat(2, 1fr);
-    }
-    .os-grid-cols-auto>.grid-col{
-      width:100%;
-    }
-    .os-grid-cols-auto>.grid-col:nth-child(1n){
-      text-align: left;
-      justify-content: left;
-    }
-    .os-grid-cols-auto>.grid-col:nth-child(2n){
-      text-align: right;
-      justify-content: right;
-    }
-    .os-grid-cols-auto>.grid-col:nth-child(2n+1):last-child{
-      grid-column:span 2;
-    }
-  }
-</style>
-
-
-
-<style scoped>
-  .os-grid-cols-auto{
-    grid-template-columns: v-bind('colWidthsStyle');
   }
   @media(max-width:767px){
     .os-grid-cols-auto{
