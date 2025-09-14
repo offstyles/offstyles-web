1b0085442e99761433938875eaceaa7c25c5eb79
why-Mia <xenonpowered@gmail.com>
2025-09-14 14:56:57 +0100

    update styling of time 'more details'

--- src/components/TimeLists/TimesListItemMoreDetails.vue
+++ src/components/TimeLists/TimesListItemMoreDetails.vue
@@ -9,25 +9,25 @@
       cols: TimeListColumn[]
     }>();
   const colWidthsStyle = computed(()=>{
-    return props.cols.map((v)=>v.width ? v.width : 'auto').join(' ');
+    return props.cols.map((v)=>v.width ? v.width : '1fr').join(' ');
   })
 </script>
 
 
 <template>
-  <div class="grid os-subgrid-cols-auto p-2 px-2 bg-main-800" :style="{ gridTemplateColumns: colWidthsStyle }">
+  <div class="grid os-subgrid-cols-auto py-3 px-2 bg-main-800" :style="{ gridTemplateColumns: colWidthsStyle }">
     <div v-for="(col, index) in props.cols" :key="index" :class="col.alignmentClasses" class="flex flex-col">
       <span class="text-gray-400 text-xs">{{ col.label }}:</span>
-      <span v-if="col.data === 'date'">
+      <span v-if="col.data === 'date'" class="monospace text-lg">
         <RelativeDate :date="props.time[col.data]" />
       </span>
-      <span v-else>{{ col.data === 'sync' ? props.time[col.data].toFixed(2) + '%' : props.time[col.data] }}</span>
+      <span class="monospace text-lg" v-else>{{ col.data === 'sync' ? props.time[col.data].toFixed(2) + '%' : props.time[col.data] }}</span>
     </div>
     <!-- Add link to individual record -->
-    <div v-if="props.time._id" class="flex flex-col justify-center">
+    <div v-if="props.time._id" class="flex flex-col justify-center items-center pt-2 pb-1 col-span-3">
       <a 
         :href="timeLinks.recordLink(props.time)" 
-        class="text-blue-400 hover:text-blue-300 text-xs px-2 py-1 bg-main-700 rounded hover:bg-main-600 transition-colors text-center"
+        class="px-8 py-2 bg-blue-700 hover:bg-blue-600 rounded transition-colors text-center text-sm"
       >
         View Record
       </a>
