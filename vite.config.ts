568f951c6e38910b4704b9406adedd33a9d05cb0
tommy <t@tommyy.dev>
2026-05-06 15:53:16 -0400

    swap replay viewer to noclip.website Source renderer

--- vite.config.ts
+++ vite.config.ts
@@ -34,7 +34,7 @@ export default defineConfig({
     port: 3000,
     proxy: {
       '/api': {
-        target: 'http://localhost:8000',
+        target: 'https://offstyles.net',
         changeOrigin: true,
         secure: true,
         cookieDomainRewrite: 'localhost',
