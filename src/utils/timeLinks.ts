6fd8c87abba2289e8e0b4cd3f648e181d7581c7d
tommy <59783653+dowoge@users.noreply.github.com>
2026-05-10 16:58:06 -0400

    preserve style of time row in player and map links (#60)

--- src/utils/timeLinks.ts
+++ src/utils/timeLinks.ts
@@ -1,10 +1,10 @@
 import type { Time } from "@/types/Time";
 
 const playerLink = function(time : Time) {
-  return `/players/${time.steamid}/`;
+  return `/players/${time.steamid}/?style=${time.style}`;
 };
 const mapLink = function(time : Time) {
-  return `/maps/${time.map}/`
+  return `/maps/${time.map}/?style=${time.style}`;
 }
 const recordLink = function(time : Time) {
   return `/run/${time._id}/`;
